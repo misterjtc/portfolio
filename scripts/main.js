ddf3ebb4e31dbfb30b9ddb06774984b1b22ec696
jordan.cook <jtcook63@gmail.com>
2019-11-13 12:54:32 -0500

    some updates)

--- scripts/main.js
+++ scripts/main.js
@@ -1,1 +1,10 @@
 // This is the main site js file
+$(document).ready(function() {
+    // Check if the DOM function is ready
+    console.log("I'm Ready")
+    // Initialize slick slider
+    $('.projectHero').slick({
+        dots: true,
+        speed: 500
+      });
+});
